65c72f652bc50c2f8f4fadfbc5819612aae56c92
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-23 00:23:42 -0700

    fixed reviews relationship

--- models/review.py
+++ models/review.py
@@ -2,7 +2,10 @@
 """This is the review class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import relationship
+from models.user import User
+from models.place import Place
 
 
 class Review(BaseModel, Base):
@@ -16,3 +19,8 @@ class Review(BaseModel, Base):
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="reviews")
+    place = relationship("Place", back_populates="reviews")
+
+User.reviews = relationship("Review", back_populates="user")
+Place.reviews = relationship("Review", back_populates="place")
