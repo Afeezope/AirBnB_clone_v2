183829159c41e6bad2feb9a98857bd82243fe7b7
Afeezope <afeezope1994@gmail.com>
2023-08-23 16:54:05 -0700

    Revert "conflict"
    This reverts commit 94def1a72c3c377ad709e9e2fb24debeae9a6f2d, reversing
    changes made to 90e039b41463dcdd196c5920a9e64ab82f9cdda5.

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ """
-import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 import pycodestyle
-import pep8
 
 
 class test_City(test_basemodel):
