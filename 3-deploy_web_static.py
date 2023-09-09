8e05021f52a1d36a988326c17e9f3b375f82ec12
Afeezope <afeezope1994@gmail.com>
2023-09-09 02:21:14 -0700

    clean_web_static file

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["100.26.133.116", "100.26.121.54"]
 
 
 def do_pack():
